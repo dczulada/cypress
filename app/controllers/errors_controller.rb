b639d8bec2f91678eff2702506a32c2d45a81bf9
David Czulada <dczulada@gmail.com>
2015-08-26 14:55:19 -0400

    switched to use nlm service instead of vsac to verify

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -8,6 +8,7 @@ class ErrorsController < ApplicationController
 
   def error_401
     respond_to do |format|
+      format.html { render template: 'errors/error_401', layout: 'layouts/application', status: 401 }
       format.all { render nothing: true, status: 401 }
     end
   end
--- app/models/vsac_user.rb
+++ app/models/vsac_user.rb
@@ -1,8 +1,15 @@
 class VsacUser
 
   def self.authenticate(username, password)
-    RestClient.post 'https://vsac.nlm.nih.gov/vsac/ws/Ticket', {username: username, password: password}
-    return true
+  	
+    #RestClient.post 'https://vsac.nlm.nih.gov/vsac/ws/Ticket', {username: username, password: password}
+    nlmResult = RestClient.post 'https://uts-ws.nlm.nih.gov/restful/isValidUMLSUser', {user: username, password: password, licenseCode: APP_CONFIG['nlm']['license_code']}
+    doc = Nokogiri::XML(nlmResult)
+    if doc.search('Result').text == 'true'
+      return true
+    else 
+      return false
+    end
     rescue Exception => e
       return false
   end
